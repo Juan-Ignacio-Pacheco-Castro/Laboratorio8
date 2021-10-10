dcb011f699959de3b2a94d0fef83eac5f2653b58
Juan-Ignacio-Pacheco-Castro <juan.pachecocastro@ucr.ac.cr>
2021-10-09 21:58:30 -0600

    Lab 8 funcionando

--- js/DataTable.js
+++ js/DataTable.js
@@ -12,7 +12,7 @@ class DataTable {
         .then(data => {
             let htmlContent = '';
 
-            let headerContent = '<thead><tr><th> Id</th><th>Quantity</th><th>Name</th><th>Price</th></tr></thead>'
+            let headerContent = '<table id="table_id" class="display"><thead><tr><th> Id</th><th>Quantity</th><th>Name</th><th>Price</th></tr></thead>'
             let row = '';
             for (const object of data) {
                 for (const key in object) {
@@ -21,8 +21,11 @@ class DataTable {
                 htmlContent += `<tr> ${row} </tr>`;
                 row = '';
             }
-            htmlContent = `${headerContent}<tbody>${htmlContent}</tbody>`;
+            htmlContent = `${headerContent}<tbody>${htmlContent}</tbody></table>`;
             this.context.innerHTML = htmlContent;
+            $(document).ready(function () {
+                $('#table_id').DataTable();
+            });
         })
         .catch(error => {
             console.log(error);
